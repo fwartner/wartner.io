cb904c174bc8b2bef58e1cd0ba7af3b28e3c0b25
Florian Wartner <florian@wartner.io>
2021-07-14 12:24:36 +0200

    Added turbolinks

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+
+var Turbolinks = require('turbolinks');
+Turbolinks.start();

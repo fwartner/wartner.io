6527a5bd4762d125cfa0bd9452e330e79210586b
Florian Wartner <florian@wartner.io>
2021-07-14 12:09:08 +0200

    Updates

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -21,6 +21,19 @@
     <!-- Scripts -->
     <script src="{{ mix('js/app.js') }}" defer></script>
     @stack('scripts')
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-E29T5QCFW7"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-E29T5QCFW7');
+    </script>
 </head>
 
 <body>

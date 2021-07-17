9c3d300a51973648c291d79082b659a3ec2a6af1
Florian Wartner <florian@wartner.io>
2021-07-17 14:16:39 +0200

    Updates

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,7 +4,7 @@
         <div class="md:flex md:items-center bg-gray-700">
             <div class="pb-5 pt-5 flex-1 min-w-0 px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                 <h2 class="text-2xl font-bold leading-7 text-white sm:text-3xl sm:truncate">
-                    Blog Posts
+                    /dev/Blog/
                 </h2>
             </div>
         </div>

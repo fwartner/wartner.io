c1af343557e78a85ddc85a88a48af28882478e4d
Florian Wartner <florian@wartner.io>
2021-07-20 18:43:57 +0200

    Updates

--- app/View/Components/Site/Footer.php
+++ app/View/Components/Site/Footer.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class site.footer extends Component
+class Footer extends Component
 {
     /**
      * Create a new component instance.

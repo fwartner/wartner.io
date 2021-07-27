b92dcc39c4b464658781cc16f61bbe70680027ea
fwartner <fwartner@users.noreply.github.com>
2021-07-27 12:43:40 +0000

    PHP CS changes

--- app/Providers/MetaTagsServiceProvider.php
+++ app/Providers/MetaTagsServiceProvider.php
@@ -2,11 +2,9 @@
 
 namespace App\Providers;
 
-use Butschster\Head\Contracts\Packages\PackageInterface;
-use Butschster\Head\Facades\PackageManager;
-use Butschster\Head\MetaTags\Meta;
 use Butschster\Head\Contracts\MetaTags\MetaInterface;
 use Butschster\Head\Contracts\Packages\ManagerInterface;
+use Butschster\Head\MetaTags\Meta;
 use Butschster\Head\Providers\MetaTagsApplicationServiceProvider as ServiceProvider;
 
 class MetaTagsServiceProvider extends ServiceProvider

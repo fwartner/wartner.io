9eabb9b8f55430c55f1874c85b614d274b9d909e
Florian Wartner <florian@wartner.io>
2021-07-27 14:23:07 +0200

    Updates

--- resources/views/components/blog/single-post.blade.php
+++ resources/views/components/blog/single-post.blade.php
@@ -46,12 +46,10 @@
         <div class="relative px-4 sm:px-6 lg:px-8">
             <div class="text-lg max-w-prose mx-auto">
                 <h1>
-                    <span class="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
-                        <time datetime="{{ $post->created_at->format('Y-m-d') }}"
-                            class="uppercase">{{ $post->created_at->format('F j, Y') }}</time>
-                    </span>
                     <span
-                        class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{{ $post->title }}</span>
+                        class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                        {{ $post->title }}
+                    </span>
                 </h1>
             </div>
             <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">

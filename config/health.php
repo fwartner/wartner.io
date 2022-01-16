980fe9a5227d7da4c5644204810aaf94634a4713
Florian Wartner <florian@wartner.io>
2022-01-16 16:57:22 +0100

    WIP

--- config/health.php
+++ config/health.php
@@ -35,7 +35,7 @@ return [
         /*
          * Notifications will only get sent if this option is set to `true`.
          */
-        'enabled' => true,
+        'enabled' => false,
 
         'notifications' => [
             Spatie\Health\Notifications\CheckFailedNotification::class => ['mail'],
